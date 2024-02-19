0106d19b9197f0b472f500ce380feb664ca8dc33
Douglas-leonardo <douglas.bsilva@contmatic.com.br>
2024-02-19 11:05:59 -0300

    Subindo front

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <main
+    >
+      <div className={styles.container}>
+        <h1>
+          Sujeito pizza
+        </h1>
+      </div>
+    </main>
+  );
+}
